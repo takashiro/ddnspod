a345b3338254912b4a1101535bf9026adc5440ed
Kazuichi Takashiro <takashiro@qq.com>
2023-08-05 23:21:18 +0800

    Update AAAA record to Tencent

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import * as os from 'os';
+import * as ip from 'ip';
+import * as tencentcloud from 'tencentcloud-sdk-nodejs-dnspod';
+
+const { Client } = tencentcloud.dnspod.v20210323;
+
+const config = require('./ddnspod.config.js');
+
+const client = new Client({
+	credential: {
+		secretId: config.secretId,
+		secretKey: config.secretKey,
+	},
+	profile: {
+		httpProfile: {
+			endpoint: 'dnspod.tencentcloudapi.com',
+		},
+	},
+});
+
+function findMyIp(): string {
+	const networks = os.networkInterfaces();
+	const configs = networks['Wi-Fi'];
+	if (!configs) {
+		throw new Error('The network interface does not exist.');
+	}
+
+	const ipv6 = configs.filter((config) => !config.internal && config.family === 'IPv6' && ip.isPublic(config.address));
+	return ipv6[0].address;
+}
+
+(async function main(): Promise<void> {
+	console.log(`Domain: ${config.domain}`);
+	console.log(`Subdomain: ${config.subdomain}`);
+
+	const myIp = findMyIp();
+	console.log(`IP: ${myIp}`);
+
+	const { RecordList: records } = await client.DescribeRecordList({
+		Domain: config.domain,
+		Subdomain: config.subdomain,
+	});
+	if (!records || records.length < 1) {
+		throw new Error('Failed to find corresponding records. Please manually create it first.');
+	}
+
+	const [record] = records;
+	await client.ModifyRecord({
+		Domain: config.domain,
+		SubDomain: config.subdomain,
+		RecordType: 'AAAA',
+		RecordId: record.RecordId,
+		RecordLine: record.Line,
+		Value: myIp,
+	});
+}());
